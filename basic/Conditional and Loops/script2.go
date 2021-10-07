209e687b562fb3c2926b17934396ebf1b5fd427f
twilight-warlock <devansh.ts@somaiya.edu>
2021-10-08 01:29:05 +0530

    Learnt about composite syntax in conditionals

---
+++ basic/Conditional and Loops/script2.go
@@ -0,0 +1,14 @@
+// Weird part in coonditionals
+package main
+
+import (
+	"fmt"
+)
+ 
+func main() {
+	if x := 100; x == 100 {
+		fmt.Println("Germany")
+	}
+}
+
+// Weird declaration concatenated in if statement known as composite syntax
