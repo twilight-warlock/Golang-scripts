bd353562f7cf37d98072b15ec2307073fcfaea13
twilight-warlock <devansh.ts@somaiya.edu>
2021-10-08 00:48:00 +0530

    Declaration with type inference script

---
+++ basic/script3.go
@@ -0,0 +1,15 @@
+// Declaration with type inference
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var i = 10
+	var s = "Canada"
+
+	fmt.Println(reflect.TypeOf(i))
+	fmt.Println(reflect.TypeOf(s))
+}
