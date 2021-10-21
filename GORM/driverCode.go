352a2a0dc2a853800b0f1fd762f312ee110650eb
twilight-warlock <shahdevansh045@gmail.com>
2021-10-22 01:26:08 +0530

    GORM basic functions added

---
+++ GORM/driverCode.go
@@ -0,0 +1,21 @@
+type User struct {
+	ID           uint
+	Name         string
+	Email        *string
+	Age          uint8
+	Birthday     *time.Time
+	MemberNumber sql.NullString
+	ActivatedAt  sql.NullTime
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+  }
+
+// gorm.Model definition
+type Model struct {
+	ID        uint           `gorm:"primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+  }
+
+  
